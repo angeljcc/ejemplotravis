420305df00b4c01108ddab809901fb5fd80bd300
Angel JCC <angeljcc@gmail.com>
2015-01-25 17:39:44 +0100

    probando

---
+++ src/ejemplotravis/prueba.php
@@ -0,0 +1,17 @@
+<?php namespace ejemplotravis;
+
+	/**
+	* 
+	*/
+	class prueba
+	{
+		
+		function __construct(argument)
+		{
+			# code...
+		}
+
+		public function add($x,$y){
+			return $x + $y;
+		}
+	}
---
+++ tests/pruebaTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+/**
+* 
+*/
+class pruebaTest extends PHPUnit_Framework_TestCase
+{
+	private $calc=null;
+
+	public function setUp(){
+		$this->calc = new ejemplotravis\prueba;
+	}
+
+	public function tearDown(){
+		$this->calc null;
+	}
+
+	public function testInstanceOf(){
+		$this->assertInstanceOf('ejemplotravis\prueba',$this->calc);
+	}
+
+	public function testInvalidArgumentisProperlyThrown(){
+		$this->calc->add('A',2);
+	}
+
+	public function testValidResult(){
+		$this->assertEquals(5,$this->calc->add(2,3));
+	}
+}
