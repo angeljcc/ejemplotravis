ee318918e2dfb54f50b423320661f0b7c20c004a
Angel JCC <angeljcc@gmail.com>
2015-01-25 17:56:30 +0100

    probando

--- tests/pruebaTest.php
+++ tests/pruebaTest.php
@@ -9,7 +9,7 @@ class pruebaTest extends PHPUnit_Framework_TestCase
 	private $calc=null;
 
 	public function setUp(){
-		$this->calc = new ejemplotravis\prueba;
+		$this->calc = new ../ejemplotravis\prueba;
 	}
 
 	public function tearDown(){
